51a159cce0403a8f7f467b84b6183021d200b91d
GT3CH1 <gavinpease@gmail.com>
2021-07-02 14:25:16 -0600

    Make changing and getting system state work

--- src/daemon.rs
+++ src/daemon.rs
@@ -4,12 +4,13 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct SysStatus {
-    state: bool,
+    system_status: bool,
 }
+
 impl SysStatus {
     fn new() -> Self {
         SysStatus {
-            state: false,
+            system_status: true,
         }
     }
 }
@@ -17,38 +18,43 @@ impl SysStatus {
 
 #[tokio::main]
 pub(crate) async fn run() {
-
-    let sys_stat = SysStatus::new();
-    let sys_stat_filter = warp::any().map(move || sys_stat.clone());
+    let sys_stat_filter_obj = SysStatus::new();
+    let sys_filter = warp::any().map(move || sys_stat_filter_obj.clone());
     let get_sys_status = warp::get()
         .and(warp::path("system"))
         .and(warp::path("state"))
         .and(warp::path::end())
-        .and(sys_stat_filter.clone())
+        .and(sys_filter.clone())
         .and_then(get_sys_status);
 
-    let set_sys_status = warp::post()
+    let set_sys_status = warp::put()
         .and(warp::path("system"))
         .and(warp::path("state"))
         .and(warp::path::end())
-        .and(sys_stat_filter.clone())
         .and(post_json())
         .and_then(set_sys_status);
 
-    let routes = get_sys_status.or(set_sys_status);
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    let routes = get_sys_status
+        .or(set_sys_status);
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
 
 fn post_json() -> impl Filter<Extract=(SysStatus, ), Error=warp::Rejection> + Clone {
-    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+    warp::body::content_length_limit(1024 * 16)
+        .and(warp::body::json())
 }
 
-async fn get_sys_status(status: SysStatus) -> Result<impl warp::Reply, warp::Rejection> {
-    let value = get_system_status(get_pool());
+async fn get_sys_status(_status: SysStatus) -> Result<impl warp::Reply, warp::Rejection> {
+    let value = SysStatus {
+        system_status: get_system_status(get_pool())
+    };
     Ok(warp::reply::json(&value))
 }
 
-async fn set_sys_status(status: SysStatus) -> Result<impl warp::Reply, warp::Rejection> {
-    set_system(get_pool(), status.state);
+async fn set_sys_status(_status: SysStatus) -> Result<impl warp::Reply, warp::Rejection> {
+    set_system(get_pool(), _status.system_status);
+    println!("{}", _status.system_status);
     Ok(warp::reply::with_status("Success", http::StatusCode::OK))
 }
